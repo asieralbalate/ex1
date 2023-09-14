6db68f56c09c1093e9bc9de7732c7caa3de9619e
asier <alualb1634@ieselcaminas.org>
2023-09-15 01:31:06 +0200

    Thrid commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,36 +5,8 @@ import java.io.File
 import java.io.InputStreamReader
 
 fun main(args: Array<String>) {
-    var f = File.listRoots()[0]
+    val f = File.listRoots()[0]
     println(llistaDirectori(f))
-    var noAcaba = true
-    var currentFile: File
-    while (noAcaba) {
-        println("Introdueix un numero (-1 per acabar): ")
-        var entrada = BufferedReader(InputStreamReader(System.`in`)).readLine()
-        var numero = entrada.toInt()
-        if (numero == -1) {
-            System.exit(0)
-        } else if (numero > f.listFiles().size || numero < -1) {
-            println("Numero incorrecte")
-        } else {
-            if (numero == 0) {
-                if (f.parentFile.exists() == null) {
-                    println(llistaDirectori(f))
-                }
-            } else {
-                currentFile = f.listFiles().sorted()[numero - 1]
-                f = currentFile
-                if (currentFile.isDirectory) {
-                    if (currentFile.canRead()) {
-                        println(llistaDirectori(currentFile))
-                    } else {
-                        println("No tens permisos")
-                    }
-                }
-            }
-        }
-    }
 }
 
 fun llistaDirectori(f: File) {
@@ -42,7 +14,11 @@ fun llistaDirectori(f: File) {
     var n = 1
     println(s)
     println("-".repeat(s.length))
-    println("${0}.- " + f.parent)
+    if (f.parent == null) {
+        println("0.- Directori pare")
+    } else {
+        println("0.- ${f.parent}")
+    }
     for (e in f.listFiles().sorted()) {
         if (e.isFile())
             println("${n}.- " + e.getName() + "\t " + e.length())
@@ -50,6 +26,46 @@ fun llistaDirectori(f: File) {
             println("${n}.- " + e.getName() + "\t <Directori>")
         }
         n++
+    }
+    println(" ")
+    println("Introdueix un numero (-1 per acabar): ")
+    val entrada = BufferedReader(InputStreamReader(System.`in`)).readLine()
+    val num = entrada.toIntOrNull()
+    if (num != null) {
+        bucleNumeros(num, f)
+    } else {
+        println("Entrada no valida. Introduix un numero válid.")
+        println(" ")
+        llistaDirectori(f)
+    }
+}
+
+fun bucleNumeros(numero: Int, f: File) {
+    var currentFile: File
+    if (numero == -1) {
+        System.exit(0)
+    } else if (numero > f.listFiles().size || numero < -1) {
+        println("Numero incorrecte")
+    } else if (numero == 0) {
+        val parentFile = f.parentFile
+        if (parentFile != null && parentFile.exists()) {
+            currentFile = f.parentFile
+            println(llistaDirectori(currentFile))
+        } else {
+            println(llistaDirectori(f))
+        }
+    } else {
+        currentFile = f.listFiles().sorted()[numero - 1]
+        if (currentFile.isDirectory) {
+            if (currentFile.canRead()) {
+                println(llistaDirectori(currentFile))
+            } else {
+                println("No tens permisos")
+                println(llistaDirectori(f))
+            }
+        } else {
+            println(llistaDirectori(f))
+        }
     }
 }
 
