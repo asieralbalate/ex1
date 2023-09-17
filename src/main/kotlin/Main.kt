52ea25c7cb55cf9c76bf017a8903994e9510a83a
asier <alualb1634@ieselcaminas.org>
2023-09-17 19:50:22 +0200

    Second H commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,15 +3,18 @@ package exemples
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
+import javax.naming.spi.DirectoryManager
 
 fun main(args: Array<String>) {
     val f = File.listRoots()[0]
-    println(llistaDirectori(f))
+    val result = listDirectory(f)
+    println(result)
 }
 
-fun llistaDirectori(f: File) {
+fun listDirectory(f: File) {
     val s = "Llista de fitxers i directoris del directori " + f.path
     var n = 1
+    println(" ")
     println(s)
     println("-".repeat(s.length))
     if (f.parent == null) {
@@ -27,46 +30,48 @@ fun llistaDirectori(f: File) {
         }
         n++
     }
+    introduceNumber(f)
+}
+
+private fun introduceNumber(f: File) {
     println(" ")
     println("Introdueix un numero (-1 per acabar): ")
-    val entrada = BufferedReader(InputStreamReader(System.`in`)).readLine()
-    val num = entrada.toIntOrNull()
+    val input = BufferedReader(InputStreamReader(System.`in`)).readLine()
+    val num = input.toIntOrNull()
     if (num != null) {
-        bucleNumeros(num, f)
+        loopNumbers(num, f)
     } else {
         println("Entrada no valida. Introduix un numero válid.")
-        println(" ")
-        llistaDirectori(f)
+        println("")
+        listDirectory(f)
     }
 }
 
-fun bucleNumeros(numero: Int, f: File) {
-    var currentFile: File
-    if (numero == -1) {
+fun loopNumbers(number: Int, f: File) {
+    val currentFile: File
+    if (number == -1) {
         System.exit(0)
-    } else if (numero > f.listFiles().size || numero < -1) {
+    } else if (number > f.listFiles().size || number < -1) {
         println("Numero incorrecte")
-    } else if (numero == 0) {
-        val parentFile = f.parentFile
-        if (parentFile != null && parentFile.exists()) {
+        println(listDirectory(f))
+    } else if (number == 0) {
+        if (f.parentFile != null && f.parentFile.exists()) {
             currentFile = f.parentFile
-            println(llistaDirectori(currentFile))
+            println(listDirectory(currentFile))
         } else {
-            println(llistaDirectori(f))
+            println(listDirectory(f))
         }
     } else {
-        currentFile = f.listFiles().sorted()[numero - 1]
-        if (currentFile.isDirectory) {
-            if (currentFile.canRead()) {
-                println(llistaDirectori(currentFile))
-            } else {
-                println("No tens permisos")
-                println(llistaDirectori(f))
-            }
+        currentFile = f.listFiles().sorted()[number - 1]
+        if (currentFile.canRead() && currentFile.canWrite() && currentFile.canExecute() && currentFile.isDirectory) {
+            println(listDirectory(currentFile))
         } else {
-            println(llistaDirectori(f))
+            if (!currentFile.canRead()) {
+                println("No tens permisos")
+                println(listDirectory(f))
+            } else {
+                println(listDirectory(f))
+            }
         }
     }
 }
-
-
